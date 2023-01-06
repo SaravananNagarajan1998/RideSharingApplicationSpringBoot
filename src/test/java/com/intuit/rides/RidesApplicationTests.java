47124e25578a9ea8c4f6867127df720177668e26
SaravananNagarajan1998 <saravanakarn99@gmail.com>
2023-01-07 00:21:36 +0530

    initial commit added.

--- src/test/java/com/intuit/rides/RidesApplicationTests.java
+++ src/test/java/com/intuit/rides/RidesApplicationTests.java
@@ -3,6 +3,8 @@ package com.intuit.rides;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+/* This is for test class*/
 @SpringBootTest
 class RidesApplicationTests {
 
