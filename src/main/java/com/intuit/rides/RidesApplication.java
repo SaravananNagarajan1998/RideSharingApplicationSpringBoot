5de9b403a69d369db92fe18d498c8274e68becc6
saravanannagarajan <saravanakarn99@gmail.com>
2023-12-03 14:20:46 +0530

    DB changes added

--- src/main/java/com/intuit/rides/RidesApplication.java
+++ src/main/java/com/intuit/rides/RidesApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RidesApplication {
 
+
+	//Starting Application
 	public static void main(String[] args) {
 		SpringApplication.run(RidesApplication.class, args);
 	}
